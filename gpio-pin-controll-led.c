77d97e19b57980643819036250b9dcfe89abf39f
Thalisson Castro <adaothalisson@hotmail.com>
2025-01-19 19:49:06 -0300

    feat: criada função que inicializa o buzzer.

--- gpio-pin-controll-led.c
+++ gpio-pin-controll-led.c
@@ -46,3 +46,19 @@ void turn_on_all_leds() {
     gpio_put(LED_GREEN, 1);
     gpio_put(LED_BLUE, 1);
 }
+
+void pwm_init_buzzer(uint pin) {
+    // Configurar o pino como saída de PWM
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+
+    // Obter o slice do PWM associado ao pino
+    uint slice_num = pwm_gpio_to_slice_num(pin);
+
+    // Configurar o PWM com frequência desejada
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, clock_get_hz(clk_sys) / (BUZZER_FREQUENCY * 4096)); // Divisor de clock
+    pwm_init(slice_num, &config, true);
+
+    // Iniciar o PWM no nível baixo
+    pwm_set_gpio_level(pin, 0);
+}
